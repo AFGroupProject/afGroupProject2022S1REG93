de75c3f1891f7929c660d5000cd343146c0a3dc6
Hithushi <hithushirangana123@gmail.com>
2022-06-04 12:01:18 +0530

    supervisor crud part

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+<<<<<<< HEAD
+=======
+require("dotenv").config();
+const panelmembers = require("./models/PanelMSchema");
+const router = require("./routes/router");
+>>>>>>> 3ae0aeb80901592109cf82762a7d218780805634
 
 const app = express();
 
@@ -11,9 +17,14 @@ const postRoutes = require('./routes/posts');
 //app middleware
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json());
 
+<<<<<<< HEAD
 //route middleware
 app.use(postRoutes);
+=======
+app.use(router);
+>>>>>>> 3ae0aeb80901592109cf82762a7d218780805634
 
 const PORT =9000;
 const DB_URL = 'mongodb+srv://nirashaj:12345@cluster0.ny2do.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App'
 
+<<<<<<< HEAD
 createRoot(document.getElementById('root')).render( 
     <App />
+=======
+createRoot(document.getElementById('root')).render(
+        <App /> 
+>>>>>>> 3ae0aeb80901592109cf82762a7d218780805634
 );
